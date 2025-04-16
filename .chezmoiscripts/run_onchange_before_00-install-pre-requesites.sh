4c374774ba372bcc298efd95edcf2b95e3b11bde
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2025-04-16 15:23:50 +0200

    feat(scripts:install): add separate pre-requisites install script

---
+++ .chezmoiscripts/run_onchange_before_00-install-pre-requesites.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script should be the first one to run so be careful if you rename it or
+# add other `run_before_` scripts
+
+set -eu
+
+# Install curl if it's not already installed
+if ! command -v curl >/dev/null; then
+  sudo apt update
+  sudo apt install -y curl
+fi
+
