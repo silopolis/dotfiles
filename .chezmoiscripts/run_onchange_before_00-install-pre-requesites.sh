57ed708e98ed9fab6083208988d4293f0516bf10
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2025-04-18 18:27:19 +0000

    feat(chezmoi:install): add build-essential to pre-requisites

--- .chezmoiscripts/run_onchange_before_00-install-pre-requesites.sh
+++ .chezmoiscripts/run_onchange_before_00-install-pre-requesites.sh
@@ -7,6 +7,6 @@ set -eu
 # Install curl if it's not already installed
 if ! command -v curl >/dev/null; then
   sudo apt update
-  sudo apt install -y curl
+  sudo apt install -y curl build-essential
 fi
 
