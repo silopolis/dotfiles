a3a41c1544a54eebcb7b75b01a5c1d4fdc0391aa
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2025-09-25 10:25:34 +0200

    fix(upgrade): test for package managers availability
    Also refactor a bit

--- dot_local/sbin/executable_sysupgrade.sh
+++ dot_local/sbin/executable_sysupgrade.sh
@@ -12,27 +12,38 @@ sudo apt -V upgrade \
     && sudo apt autoremove \
     && sudo apt clean
 
-echo "-- Update Brew package sources"
-brew update -v \
-    && echo "-- Upgrade Brew packages" \
-    && brew upgrade
+command -v brew > /dev/null \
+  && echo "-- Update brew package sources" \
+  && brew update -v \
+  && echo "-- Upgrade brew packages" \
+  && brew upgrade
 
-echo "-- Upgrade pipx packages"
-pipx upgrade-all --include-injected
+command -v pipx > /dev/null \
+  && echo "-- Upgrade pipx packages" \
+  && pipx upgrade-all --include-injected \
+  || echo "-- pipx package manager not available"
 
-echo "-- Upgrade bin managed packages"
-bin update --all --continue-on-error #--yes
+command -v bin > /dev/null \
+  && echo "-- Upgrade bin managed packages" \
+  && bin update --all --continue-on-error \
+  || echo "-- bin package manager not available"
 
-echo "-- Update Flatpack packages"
-flatpak update
+command -v flatpack > /dev/null \
+  && echo "-- Update Flatpack packages" \
+  && flatpak update \
+  || echo "-- flatpack package manager not available"
 
-#echo "-- Update Snap packages"
-#snap refresh
-echo "--Update gh CLI extensions"
-gh extensions upgrade --all
+command -v snap > /dev/null \
+  && echo "-- Update Snap packages" \
+  && snap refresh \
+  && echo "-- Clean disabled Snap packages" \
+  && snap-clean \
+  || echo "-- snap package manager not available"
 
-echo "-- Clean disabled Snap packages"
-snap-clean
+command -v gh > /dev/null \
+  && echo "-- Update gh CLI extensions" \
+  && gh extensions upgrade --all \
+  || echo "-- gh command not available"
 
 echo "-- Clean up Journal"
 sudo journalctl --vacuum-size=256M
